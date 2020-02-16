8535dc09cc90f52df188b5965e3009b37ecb98c7
Hasibul Hasan Shanto <hasibulhasan.kajal@gmail.com>
2020-02-16 21:29:30 +0600

    Update

--- vue.config.js
+++ vue.config.js
@@ -1,25 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/vue-todo-app/'
-    : '/'
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/vue-todo-app/'
+        : '/'
 }
-const path = require("path");
-
-module.exports = {
-  chainWebpack: config => {
-    const types = ["vue-modules", "vue", "normal-modules", "normal"];
-    types.forEach(type =>
-      addStyleResource(config.module.rule("stylus").oneOf(type))
-    );
-  }
-};
-
-function addStyleResource(rule) {
-  rule
-    .use("style-resource")
-    .loader("style-resources-loader")
-    .options({
-      patterns: [path.resolve(__dirname, "./src/styles/imports.styl")]
-    });
-}
-
